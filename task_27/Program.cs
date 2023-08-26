b9b4abb393ee00eb1a41a9c00634a3be4b5666ad
Vyacheslav_Zyablov <asyofka@mail.ru>
2023-08-26 13:44:30 +0300

    task_27 done

--- task_27/Program.cs
+++ task_27/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int EnterNum(string phrase)
+{
+    Console.WriteLine(phrase);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int introdusedNumber = EnterNum("Введите любое (желательно многозначное) число: ");
+
+int theSumOfThePartsOfANumber(int number)
+{
+    int result = 0;
+    int temp = 0;
+    for (int i = 0; number > 0; i++)
+    {
+        temp = number % 10;
+        number /= 10;
+        result += temp;
+    }
+    return result;
+}
+
+System.Console.WriteLine($"Сумма элементов числа {introdusedNumber} равна: {theSumOfThePartsOfANumber(introdusedNumber)}");
