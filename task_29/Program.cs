036ad8c22d79370587fbde72cc77c9f97d9770d1
Vyacheslav_Zyablov <asyofka@mail.ru>
2023-08-26 15:03:12 +0300

    task_29 done

--- task_29/Program.cs
+++ task_29/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33-> [6, 1, 33]
+))) Не ясно по примеру, сделал по условию 
+*/
+
+int[] array = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    int EnterNum(string phrase)
+    {
+        Console.WriteLine(phrase);
+        return int.Parse(Console.ReadLine()!);
+    }
+    int numbersForArray = EnterNum($"Введите число: {i + 1} элемента массива ");
+    array[i] = numbersForArray;
+}
+
+Console.Write($"Элементы массива: ");
+for (int i = 0; i < array.Length; i++)
+{
+
+    Console.Write(array[i]);
+    if (i < array.Length - 1) Console.Write(", ");
+}
+
+
+
+
+
+
+
